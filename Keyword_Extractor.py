485698ed515b80ade62daac7ddb35ebb23b169ec
danielgulloa <danielgulloa@gmail.com>
2018-02-12 19:38:28 -0500

    Included Cosine Similarity

--- Keyword_Extractor.py
+++ Keyword_Extractor.py
@@ -4,6 +4,7 @@ import sys, os
 import numpy as np
 import nltk
 import operator
+import math
 
 class Extractor():
 	def __init__(self):
@@ -11,6 +12,7 @@ class Extractor():
 		self.hardskills=self.load_skills('hardskills.txt')
 		self.jb_distribution=self.build_ngram_distribution(sys.argv[-2])
 		self.cv_distribution=self.build_ngram_distribution(sys.argv[-1])
+		self.table=[]
 		self.outFile="Extracted_keywords.csv"
 
 	def load_skills(self,filename):
@@ -59,30 +61,52 @@ class Extractor():
 
 	def measure2(self,v1,v2):
 		return max(v1-v2,0)
+
+	def measure3(self,v1,v2):#cosine similarity
+		#"compute cosine similarity of v1 to v2: (v1 dot v2)/{||v1||*||v2||)"
+    		sumxx, sumxy, sumyy = 0, 0, 0
+    		for i in range(len(v1)):
+        		x = v1[i]; y = v2[i]
+        		sumxx += x*x
+        		sumyy += y*y
+        		sumxy += x*y
+    		return sumxy/math.sqrt(sumxx*sumyy)
+
+
 		
 
-	def sendToFile(self,table):
+	def sendToFile(self):
 		try:
     			os.remove(self.outFile)
 		except OSError:
     			pass
-		df=pd.DataFrame(table,columns=['type','skill','job','cv','m1','m2'])
+		df=pd.DataFrame(self.table,columns=['type','skill','job','cv','m1','m2'])
 		df_sorted=df.sort_values(by=['job','cv'], ascending=[False,False])
 		df_sorted.to_csv(self.outFile)
 
-	def printMeasures(self,table):		
-		v1=[table[m1][4] for m1 in range(len(table))]
-		v2=[table[m2][5] for m2 in range(len(table))]
+	def printMeasures(self):
+		n_rows=len(self.table)		
+		v1=[self.table[m1][4] for m1 in range(n_rows)]
+		v2=[self.table[m2][5] for m2 in range(n_rows)]
 		print("Measure 1: ",str(sum(v1)))
 		print("Measure 2: ",str(sum(v2)))
 		
+		v1=[self.table[jb][2] for jb in range(n_rows)]
+		v2=[self.table[cv][3] for cv in range(n_rows)]
+		print("Measure 3 (cosine sim): ",str(self.measure3(v1,v2)))
+			
+		for type in ['hard','soft','general']:
+			v1=[self.table[jb][2] for jb in range(n_rows) if self.table[jb][0]==type]
+			v2=[self.table[cv][3] for cv in range(n_rows) if self.table[cv][0]==type]
+			print("Cosine similarity for "+type+" skills: "+str(self.measure3(v1,v2)))		
+
 
 	def makeTable(self):
 		
 		#I am interested in verbs, nouns, adverbs, and adjectives
 		parts_of_speech=['CD','JJ','JJR','JJS','MD','NN','NNS','NNP','NNPS','RB','RBR','RBS','VB','VBD','VBG','VBN','VBP','VBZ']
 		graylist=["you", "will"]
-		table=[]
+		tmp_table=[]
 		#look if the skills are mentioned in the job description and then in your cv
 		
 		for skill in self.hardskills:
@@ -94,7 +118,7 @@ class Extractor():
 					count_cv=0
 				m1=self.measure1(count_jb,count_cv)
 				m2=self.measure2(count_jb,count_cv)
-				table.append(['hard',skill,count_jb,count_cv,m1,m2])
+				tmp_table.append(['hard',skill,count_jb,count_cv,m1,m2])
 
 		for skill in self.softskills:
 			if skill in self.jb_distribution:
@@ -105,7 +129,7 @@ class Extractor():
 					count_cv=0
 				m1=self.measure1(count_jb,count_cv)
 				m2=self.measure2(count_jb,count_cv)
-				table.append(['soft',skill,count_jb,count_cv,m1,m2])
+				tmp_table.append(['soft',skill,count_jb,count_cv,m1,m2])
 						
 
 		#And now for the general language of the job description:
@@ -126,16 +150,18 @@ class Extractor():
 					count_cv=0
 				m1=self.measure1(count_jb,count_cv)
 				m2=self.measure2(count_jb,count_cv)
-				table.append(['general',skill,count_jb,count_cv,m1,m2])
-		return table
+				tmp_table.append(['general',skill,count_jb,count_cv,m1,m2])
+		self.table=tmp_table
 
 
 
 def main():
 	K=Extractor()
-	table=K.makeTable()
-	K.sendToFile(table)
-	K.printMeasures(table)
+	K.makeTable()
+	K.sendToFile()
+	K.printMeasures()
+
+
 
 if __name__ == "__main__":
     main()
